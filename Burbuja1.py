8e73cbda6c124f9a5624b1576155b844ae6b3aba
KarenEOG <30853175+KarenEOG@users.noreply.github.com>
2017-09-01 11:59:17 -0500

    Add files via upload

---
+++ Burbuja1.py
@@ -0,0 +1,13 @@
+
+def burbuja(A):
+	for i in range(1,len(A)):
+		for j in range(0,len(A)-1):
+			if(A[j+1]<A[j]):
+				aux=A[j]
+				A[j]=A[j+1]
+				A[j+1]=aux
+	print(A)
+
+a=[6,5,3,1,8,7,2,4]
+print(A)
+burbuja(A)
---
+++ Insertion2.py
@@ -0,0 +1,11 @@
+def ord_insercion(lista):
+    for indice in range(1,len(lista)):
+        valor=lista[indice]
+        i=indice-1
+        while i>=0:
+            if valor<lista[i]
+                lista[i+1]=lista[i]
+		lista[i]=valor
+		i=i-1
+	    else:
+		break
