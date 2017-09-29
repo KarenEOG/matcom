83b7c8d2008f98169f2e577ca19a83b257d08be9
KarenEOG <30853175+KarenEOG@users.noreply.github.com>
2017-09-29 11:51:14 -0500

    Add files via upload

---
+++ Pila.py
@@ -0,0 +1,22 @@
+class Pila:
+	def _init_ ():
+		self.pila = []
+
+	def obtener ():
+		return self.pila.pop()
+	def i(e)
+		self.pila.push(e)
+		return len(self.pila)
+	@property
+	def longitud(self):
+		return len(self.pila)
+
+p=Pila()
+p.meter(1)
+p.meter(2)
+p.meter(2)
+p.meter(3)
+p.meter(100)
+p.meter()
+print(p.longitd)
+print(p.obterner())
