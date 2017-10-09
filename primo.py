b0af3ab73fdbb0ee0da67c90e7842f8f6eaf2f88
KarenEOG <30853175+KarenEOG@users.noreply.github.com>
2017-10-09 11:41:28 -0500

    Add files via upload

---
+++ primo.py
@@ -0,0 +1,21 @@
+def primo(n):
+	cnt=0
+	for i in range(2,round(n**.5)):
+		cnt=cnt+1
+		if((n%i)==0):
+			break
+	return cnt
+
+for i in range(1,100):
+	print(primo(i))
+
+
+numero=[]
+operaciones=[]
+for i in range(1,10000,50):
+	numero.append(i)
+	operaciones.append(primo8i))
+
+numero
+operaciones
+
