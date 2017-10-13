5edea36c67c1e9411069cf7b151db6f7c9e8cd82
KarenEOG <30853175+KarenEOG@users.noreply.github.com>
2017-10-13 11:31:41 -0500

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+cnt=0
+
+def fibonacci(n):
+	global cnt
+	cnt=cnt+1
+	if n==0 or n==1:
+		return(1)
+	return fibonacci(n-2) + fibonacci(n-1)
+
+for i in range(1,50):
+	cnt=0
+	fibonacci(i)
+	print(i,",",cnt)
